21c63de3e202dba70cf9364ca89d1f2b97b0e6dc
lumasia07 <lumasiastancelaus@gmail.com>
2023-10-12 11:51:48 -0700

    squraes

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - function to print square
+ * @size: size to be printed
+ * Return: Always 0 (Success)
+ */
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+}

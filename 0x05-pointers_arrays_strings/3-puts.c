bf0b451ebad4835ce5634f65b569e7390243ab71
lumasia07 <lumasiastancelaus@gmail.com>
2023-10-16 07:14:54 -0700

    puts fix

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,9 +9,9 @@ void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		_puts(*str);
+		_putchar(*str);
 		str++;
 	}
 
-	_puts('\n');
+	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ void reset_to_98(int *n);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
 void _puts(char *str);
+int _putchar(char c);
 
 #endif

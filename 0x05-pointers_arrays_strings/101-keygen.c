20cc9f64d7e69d9cb03df0d1d8d352774ff1f8d8
lumasia07 <lumasiastancelaus@gmail.com>
2023-10-16 13:36:25 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - genarates random passwords
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int pass[100];
+	int n, x, sum;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (x = 0; x < 100; x++)
+	{
+		pass[x] = rand() % 78;
+		sum += (pass[x] + '0');
+		putchar(pass[x] + '0');
+
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+}

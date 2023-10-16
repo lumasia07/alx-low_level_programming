585556b1a2cf58a01a0770e87785d27c5a4d8cbb
lumasia07 <lumasiastancelaus@gmail.com>
2023-10-16 09:27:10 -0700

    rev fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,17 +7,22 @@
 void rev_string(char *s)
 {
 	char tmp;
-	int i, len;
+	int i, len, len_1;
+
+	len = 0;
+	len_1 = 0;
 
 	while (s[len] != '\0')
 	{
 		len++;
 	}
 
+	len = len - 1;
+
 	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
-		s[i] = s[len - 1 - i];
-		s[len - 1 - i] = tmp;
+		s[i] = s[len_1];
+		s[len_1--] = tmp;
 	}
 }

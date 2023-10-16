4c71db47448a6ce5e50acec41001da2318522664
lumasia07 <lumasiastancelaus@gmail.com>
2023-10-16 07:32:17 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string followed by an new line
+ * @s: pointer to char
+ * Return: Always 0 (Success)
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != 0)
+	{
+		len++;
+	}
+
+	for (int i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}

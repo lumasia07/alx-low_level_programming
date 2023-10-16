9f66eb6b8b55a869a33a6d872a33b5538d08419d
lumasia07 <lumasiastancelaus@gmail.com>
2023-10-16 10:37:27 -0700

    last half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half a string
+ * @str: pointer to char
+ */
+void puts_half(char *str)
+{
+	int i, len;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		i = len / 2;
+	}
+	else
+	{
+		i = (len - 1) / 2;
+	}
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n')
+}

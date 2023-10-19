5ee46effa48045d23c9aeae44f62c1b8f537fcfe
lumasia07 <lumasiastancelaus@gmail.com>
2023-10-19 08:30:53 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string
+ * @str: string to be encoded
+ * Return: encoded string
+ */
+char *rot13(char *str)
+{
+	int i, j;
+
+	char x[] = "ABCDEGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	char y[] = "NOPQRGUTVWXYZABCDEFGHIJKLMnopqrgutvwxyzabcdefgijklm"
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0')
+		{
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}

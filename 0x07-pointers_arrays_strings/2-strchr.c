1fac7b3950e20efe5b5f718916669d77c6228062
lumasia07 <lumasiastancelaus@gmail.com>
2023-10-23 09:17:43 -0700

    fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,18 +8,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s)
-	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
-	}
-	if (!c)
-	{
-		return (s);
-	}
-	return (NULL);
-}
+	int i;
 
+	i = 0;
+
+	while (s[i] && s[i] != c)
+	{
+		i++;
+	}
+
+	return (s[i] == c ? s : NULL);
+}

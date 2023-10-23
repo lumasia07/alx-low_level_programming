6a0d9882386e75ad25385ef52c252b78879974b7
lumasia07 <lumasiastancelaus@gmail.com>
2023-10-23 11:50:34 -0700

    str fix

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: pointer to string
+ * @needle: pointer to substring
+ * Return: pointer to the beginning of located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int x, y;
+
+	for (x = 0; haystack[x] != '\0'; x++)
+	{
+		for (y = 0; needle[y] != '\0'; y++)
+		{
+			if (haystack[x + y] != needle[y])
+				break;
+		}
+		if (!needle[y])
+		{
+			return (&haystack[x]);
+		}
+	}
+	return (NULL);
+}

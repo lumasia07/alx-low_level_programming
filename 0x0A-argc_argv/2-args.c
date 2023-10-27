2c80f9e3a0445768f61ed5a1cf17dda3b42b01e7
lumasia07 <lumasiastancelaus@gmail.com>
2023-10-27 15:08:38 -0700

    2 args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all argsit receives
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	printf("\n");
+
+	return (0);
+}

f9e685ced701860c41e6cba6d0ee34d4a9193570
lumasia07 <lumasiastancelaus@gmail.com>
2023-11-08 10:22:20 -0800

    if fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,13 +12,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int x;
 
-	if (array && cmp)
-	{
+	if (array == NULL & cmp == NULL)
+		return (-1);
+
 		for (x = 0; x < size; x++)
 		{
 			if (cmp(array[x]) != 0)
 				return (x);
 		}
-	}
 	return (-1);
 }

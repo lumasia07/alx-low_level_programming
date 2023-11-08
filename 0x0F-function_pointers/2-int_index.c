575869fa816de8f6be37009cab1673eee887f984
lumasia07 <lumasiastancelaus@gmail.com>
2023-11-08 06:56:07 -0800

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_index - searches an integer
+ * @array: array to be searched
+ * @size: size of array
+ * @cmp: pointr to func
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (array && comp)
+	{
+		for (x = 0; x < size; x++)
+		{
+			if(cmp(array[x] != 0)
+					return (x);
+					
+		}
+	}
+	return (-1);
+}

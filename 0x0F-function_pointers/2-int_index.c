3aefb97dd14e21ae6eebf288f13c8316966567e2
lumasia07 <lumasiastancelaus@gmail.com>
2023-11-08 10:28:26 -0800

    added size

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int x;
 
-	if (array == NULL & cmp == NULL)
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 
 		for (x = 0; x < size; x++)

d111a406bdd64564e7d886a264a4ad99faa701cb
lumasia07 <lumasiastancelaus@gmail.com>
2023-11-08 10:13:09 -0800

    fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,18 +5,18 @@
  * @array: array to be searched
  * @size: size of array
  * @cmp: pointr to func
+ * Return: index of first element, otherwise -1
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int x;
 
-	if (array && comp)
+	if (array && cmp)
 	{
 		for (x = 0; x < size; x++)
 		{
-			if(cmp(array[x] != 0)
+			if (cmp(array[x] != 0))
 				return (x);
-					
 		}
 	}
 	return (-1);

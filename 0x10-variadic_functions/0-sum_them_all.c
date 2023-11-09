d1beb8a2dcbe3bb6f6e88f6e9a4dc1fe03c2bc6b
lumasia07 <lumasiastancelaus@gmail.com>
2023-11-09 01:53:31 -0800

    new

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns sumof all its params
+ * @n: nuumber of args
+ * Return: sum of params, else 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+		return (0);
+
+	int sum == 0;
+	unsigned int i = 0;
+	va_list args;
+
+	va_start(args, n);
+
+	while (i < n)
+	{
+		sum += va_args(args, int);
+		i++;
+	}
+
+	va_end(args);
+
+	return (sum);
+}
+
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,2 @@
+This is a project is on variadic functions
+Functions thst accept indef no. of funcs

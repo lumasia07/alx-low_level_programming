5dbd015630079b3332ae8f479d0e0b161d591dd7
lumasia07 <lumasiastancelaus@gmail.com>
2023-11-16 03:45:27 -0800

    betty fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,6 +4,7 @@
  * add_node - adds a new node to beginning of node
  * @head: first node
  * @str: pointer to a const str
+ * Return: address of new element
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -18,16 +19,17 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
-	
+
 	add->str = strdup(str);
-	if(add->str == NULL)
+
+	if (add->str == NULL)
 	{
 		free(add);
 		return (NULL);
 	}
 
 	add->len = 0;
-	
+
 	while (str[add->len] != '\0')
 	{
 		add->len++;

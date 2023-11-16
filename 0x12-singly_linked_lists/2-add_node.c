5d4ecb783c1868625da212c3c05387248f65841a
lumasia07 <lumasiastancelaus@gmail.com>
2023-11-16 03:33:15 -0800

    added a new node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node to beginning of node
+ * @head: first node
+ * @str: pointer to a const str
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *add;
+
+	if (str == NULL)
+		return (NULL);
+
+	add = malloc(sizeof(list_t));
+
+	if (add == NULL)
+	{
+		return (NULL);
+	}
+	
+	add->str = strdup(str);
+	if(add->str == NULL)
+	{
+		free(add);
+		return (NULL);
+	}
+
+	add->len = 0;
+	
+	while (str[add->len] != '\0')
+	{
+		add->len++;
+	}
+
+	add->next = *head;
+	*head = add;
+
+	return (add);
+}

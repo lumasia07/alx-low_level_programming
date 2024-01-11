417b75e1e96d119d446d0035f2822eec25ed4b60
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-11 01:23:42 -0800

    betty fixes

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -3,7 +3,7 @@
 /**
  * add_dnodeint_end - add a new node at the end
  * @head: pointer to beginning of list
- * n: new node data
+ * @n: new node data
  * Return: address of new node, NULL if failed
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)

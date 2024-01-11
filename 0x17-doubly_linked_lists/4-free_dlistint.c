6ad1f56b0db4815456f9098091d12fd15aeecfa3
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-11 01:34:27 -0800

    free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a list
+ * @head: first node of list
+ * Return: Always 0
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *now = *head;
+	dlistint *next;
+
+	while (now != NULL)
+	{
+		next = now->next;
+		free(now);
+		now = next;
+	}
+}

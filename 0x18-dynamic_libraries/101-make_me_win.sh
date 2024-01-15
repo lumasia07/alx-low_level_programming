dba38c5fde23d034ee7930a85c0c62989aa508ed
lumasia07 <lumasiastancelaus@gmail.com>
2024-01-15 02:14:05 -0800

    fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/lumasia07/alx-low_level_programming/raw/master/0x17-dynamic_libraries/loterry.so
+wget -P /tmp https://github.com/lumasia07/alx-low_level_programming/blob/master/0x17-dynamic_libraries/loterry.so
 export LD_PRELOAD=/tmp/lottery.so

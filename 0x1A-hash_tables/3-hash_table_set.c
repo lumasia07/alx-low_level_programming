3891ba22277012724385ae491ec4c6c4eec06d40
lumasia07 <lumasiastancelaus@gmail.com>
2024-02-02 02:16:10 -0800

    betty fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,35 +16,35 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
 		return (0);
 
-	 val_cp = strdup(value);
-	 if (val_cp == NULL)
-		 return (0);
+	val_cp = strdup(value);
+	if (val_cp == NULL)
+		return (0);
 
-	 idx = key_index((const unsigned char *)key, ht->size);
-	 for (i = idx; ht->array[i]; i++)
-	 {
-		 if (strcmp(ht->array[i]->key, key) == 0)
-		 {
-			 free(ht->array[i]->value);
-			 ht->array[i]->value = val_cp;
-			 return (1);
-		 }
-	 }
+	idx = key_index((const unsigned char *)key, ht->size);
+	for (i = idx; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = val_cp;
+			return (1);
+		}
+	}
 
-	 new_node = malloc(sizeof(hash_node_t));
-	 if (new_node == NULL)
-	 {
-		 free(val_cp);
-		 return (0);
-	 }
-	 new_node->key = strdup(key);
-	 if (new_node == NULL)
-	 {
-		 free(new_node);
-		 return (0);
-	 }
-	 new_node->value = val_cp;
-	 new_node->next = ht->array[idx];
-	 ht->array[idx] = new_node;
-	 return (1);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(val_cp);
+		return (0);
+	}
+	new_node->key = strdup(key);
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = val_cp;
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
+	return (1);
 }
